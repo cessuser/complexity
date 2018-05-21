b59f474e51c5c3208b935c1d916c971e310ed99b
shinepans <shinepans@live.com>
2018-05-21 15:34:09 +0800

    done

--- die_game/pages.py
+++ die_game/pages.py
@@ -1,24 +1,29 @@
 from otree.api import Currency as c, currency_range
 from ._builtin import Page, WaitPage
 from .models import Constants
+from otree.forms.widgets import _CurrencyInput
+
+class Instructions(Page):
+    def vars_for_template(self):
+        return {'curname':_CurrencyInput.CURRENCY_SYMBOL}
 
 
-class MyPage(Page):
-    pass
+class DiceRolling(Page):
+    form_model = 'player'
+    form_fields = ['dice']
+
+    def before_next_page(self):
+        self.player.set_payoff()
 
 
-class ResultsWaitPage(WaitPage):
+class DiceRollingResults(Page):
+    ...
 
-    def after_all_players_arrive(self):
-        pass
-
-
-class Results(Page):
-    pass
 
 
 page_sequence = [
-    MyPage,
-    ResultsWaitPage,
-    Results
+    Instructions,
+    DiceRolling,
+    DiceRollingResults,
+
 ]
--- risk_pref_bak/models.py
+++ risk_pref_bak/models.py
@@ -3,20 +3,18 @@ from otree.api import (
     Currency as c, currency_range
 )
 
-author = 'Your name here'
+
+author = 'Philipp Chapkovski'
 
 doc = """
-Your app description
+R.Duch, D. Landa project on die_game
 """
 
 
-
-
 class Constants(BaseConstants):
-    name_in_url = 'die_game'
+    name_in_url = 'risk_pref'
     players_per_group = None
     num_rounds = 1
-    report_coef = 100
 
 
 class Subsession(BaseSubsession):
@@ -28,8 +26,4 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    dice = models.IntegerField(min=1, max=6,
-                               verbose_name='Please report the number on the dice')
-
-    def set_payoff(self):
-        self.payoff = Constants.report_coef * self.dice
+    pass
